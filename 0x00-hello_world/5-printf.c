d847d17a4bea67bc7d0bb9c632252f6f9c1ee316
420scar <ibrahimabdi027@outlook.com>
2023-02-16 02:26:41 -0800

    a program for printing using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a program for printing a line using prinf
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (o);
+}
