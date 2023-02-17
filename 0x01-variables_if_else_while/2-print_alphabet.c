d4cffe0ff4f7978ab57d4baa4d211246935f25e5
420scar <ibrahimabdi027@outlook.com>
2023-02-17 13:36:24 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\');
+	return (0);
+}
