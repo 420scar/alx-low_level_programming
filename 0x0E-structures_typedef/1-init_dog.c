a5c156d0dbc84423adfd4e3b68a8a39012ac9d57
420scar <ibrahimabdi027@outlook.com>
2023-03-21 10:37:55 -0700

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,8 +8,7 @@
 * @owner: owner to initialize
 * Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
 */
-
-	void init_dog(struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
 	if (d == NULL)
